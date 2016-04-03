49b98a60667843a3bd349bfb959b4d9428874189
Lionel Flandrin <lionel.flandrin@gmail.com>
2016-04-03 02:49:15 +0200

    Implemented bilinear texture filtering

--- src/renderer/shaders/command_fragment.glsl
+++ src/renderer/shaders/command_fragment.glsl
@@ -58,6 +58,60 @@ const int dither_pattern[16] =
           -3,  1, -4,  0,
            3, -1,  2, -2);
 
+vec4 sample_texel(vec2 coords) {
+
+  // Number of texel per VRAM 16bit "pixel" for the current depth
+  uint pix_per_hw = 1U << frag_depth_shift;
+
+  // 8 and 4bpp textures contain several texels per 16bit VRAM
+  // "pixel"
+  float tex_x_float = coords.x / float(pix_per_hw);
+
+  // Texture pages are limited to 256x256 pixels
+  int tex_x = int(tex_x_float) & 0xff;
+  int tex_y = int(coords.y) & 0xff;
+
+  tex_x += int(frag_texture_page.x);
+  tex_y += int(frag_texture_page.y);
+
+  vec4 texel = vram_get_pixel(tex_x, tex_y);
+
+  if (frag_depth_shift > 0U) {
+    // 8 and 4bpp textures are paletted so we need to lookup the
+    // real color in the CLUT
+
+    uint icolor = rebuild_psx_color(texel);
+
+    // A little bitwise magic to get the index in the CLUT. 4bpp
+    // textures have 4 texels per VRAM "pixel", 8bpp have 2. We need
+    // to shift the current color to find the proper part of the
+    // halfword and then mask away the rest.
+
+    // Bits per pixel (4 or 8)
+    uint bpp = 16U >> frag_depth_shift;
+
+    // 0xf for 4bpp, 0xff for 8bpp
+    uint mask = ((1U << bpp) - 1U);
+
+    // 0...3 for 4bpp, 1...2 for 8bpp
+    uint align = uint(fract(tex_x_float) * pix_per_hw);
+
+    // 0, 4, 8 or 12 for 4bpp, 0 or 8 for 8bpp
+    uint shift = (align * bpp);
+
+    // Finally we have the index in the CLUT
+    uint index = (icolor >> shift) & mask;
+
+    int clut_x = int(frag_clut.x + index);
+    int clut_y = int(frag_clut.y);
+
+    // Look up the real color for the texel in the CLUT
+    texel = vram_get_pixel(clut_x, clut_y);
+  }
+
+  return texel;
+}
+
 void main() {
 
   vec4 color;
@@ -66,62 +120,37 @@ void main() {
     color = vec4(frag_shading_color, 0.);
   } else {
     // Look up texture
-
-    // Number of texel per VRAM 16bit "pixel" for the current depth
-    uint pix_per_hw = 1U << frag_depth_shift;
-
-    // 8 and 4bpp textures contain several texels per 16bit VRAM
-    // "pixel"
-    float tex_x_float = frag_texture_coord.x / float(pix_per_hw);
-
-    // Texture pages are limited to 256x256 pixels
-    int tex_x = int(tex_x_float) & 0xff;
-    int tex_y = int(frag_texture_coord.y) & 0xff;
-
-    tex_x += int(frag_texture_page.x);
-    tex_y += int(frag_texture_page.y);
-
-    vec4 texel = vram_get_pixel(tex_x, tex_y);
-
-    if (frag_depth_shift > 0U) {
-      // 8 and 4bpp textures are paletted so we need to lookup the
-      // real color in the CLUT
-
-      uint icolor = rebuild_psx_color(texel);
-
-      // A little bitwise magic to get the index in the CLUT. 4bpp
-      // textures have 4 texels per VRAM "pixel", 8bpp have 2. We need
-      // to shift the current color to find the proper part of the
-      // halfword and then mask away the rest.
-
-      // Bits per pixel (4 or 8)
-      uint bpp = 16U >> frag_depth_shift;
-
-      // 0xf for 4bpp, 0xff for 8bpp
-      uint mask = ((1U << bpp) - 1U);
-
-      // 0...3 for 4bpp, 1...2 for 8bpp
-      uint align = uint(fract(tex_x_float) * pix_per_hw);
-
-      // 0, 4, 8 or 12 for 4bpp, 0 or 8 for 8bpp
-      uint shift = (align * bpp);
-
-      // Finally we have the index in the CLUT
-      uint index = (icolor >> shift) & mask;
-
-      int clut_x = int(frag_clut.x + index);
-      int clut_y = int(frag_clut.y);
-
-      // Look up the real color for the texel in the CLUT
-      texel = vram_get_pixel(clut_x, clut_y);
-    }
+    vec4 texel_00 = sample_texel(vec2(frag_texture_coord.x, frag_texture_coord.y));
 
     // texel color 0x0000 is always fully transparent (even for opaque
     // draw commands)
-    if (is_transparent(texel)) {
+    if (is_transparent(texel_00)) {
       // Fully transparent texel, discard
       discard;
     }
+
+    // bilinear filtering
+    vec4 texel_10 = sample_texel(vec2(frag_texture_coord.x + 1, frag_texture_coord.y + 0));
+    vec4 texel_01 = sample_texel(vec2(frag_texture_coord.x + 0, frag_texture_coord.y + 1));
+    vec4 texel_11 = sample_texel(vec2(frag_texture_coord.x + 1, frag_texture_coord.y + 1));
+
+    if (is_transparent(texel_10)) {
+      texel_10 = texel_00;
+    }
+
+    if (is_transparent(texel_01)) {
+      texel_01 = texel_00;
+    }
+
+    if (is_transparent(texel_11)) {
+      texel_11 = texel_01;
+    }
+
+    float u_frac = fract(frag_texture_coord.x);
+    float v_frac = fract(frag_texture_coord.y);
+
+    vec4 texel = (texel_00 * (1 - u_frac) + texel_10 * u_frac) * (1 - v_frac)
+      + (texel_01 * (1 - u_frac) + texel_11 * u_frac) * v_frac;
 
     if (frag_texture_blend_mode == BLEND_MODE_RAW_TEXTURE) {
       color = texel;
